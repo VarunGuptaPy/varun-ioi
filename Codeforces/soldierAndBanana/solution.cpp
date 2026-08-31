45d9d35ce9e5a030043c060e99d070aba0ecb948
VarunGuptaPy <varun.gupta.py@gmail.com>
2026-08-31 16:03:26 +0530

    soldierAndBanana

---
+++ Codeforces/soldierAndBanana/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using vi = vector<int>;
+
+#define all(x) begin(x), end(x)
+#define rall(x) rbegin(x), rend(x)
+
+void solve() {
+    int k,n,w;
+    cin >> k >> n >> w;
+    ll price = k* (w*(w+1))/2;
+    if (price > n){
+        cout << price - n;
+    } else {
+        cout << 0;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test_cases = 1;
+    // cin >> test_cases;
+
+    while (test_cases--) {
+        solve();
+    }
+
+    return 0;
+}
