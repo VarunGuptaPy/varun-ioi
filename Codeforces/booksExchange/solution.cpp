070ab13d1e23bf0b558484691c3bfde552028582
VarunGuptaPy <varun.gupta.py@gmail.com>
2026-09-21 09:42:28 +0530

    booksExchangeV2

--- Codeforces/booksExchange/solution.cpp
+++ Codeforces/booksExchange/solution.cpp
@@ -8,31 +8,51 @@ using vi = vector<int>;
 #define all(x) begin(x), end(x)
 #define rall(x) rbegin(x), rend(x)
 
-int getDays(vector<int> &changes, int originalNum, int currentNum){
-    if (originalNum == currentNum){
-        return 1;
-    }
-    int days = 1 + getDays(changes,originalNum,changes[currentNum-1]);
-    return days;
-}
+
 
 void solve() {
     int n;
     cin >> n;
+
     vector<int> changes(n);
-    for (int i = 0; i<n ;i++){
+
+    for (int i = 0; i < n; i++) {
         cin >> changes[i];
     }
-    vector<int> totalDays(n);
-    for (int i = 1; i<=n; i++){
-        totalDays[i-1] = getDays(changes, i, changes[i-1]);
+
+    vector<int> ans(n);
+    vector<bool> visited(n, false);
+
+    for (int i = 0; i < n; i++) {
+
+        if (visited[i])
+            continue;
+
+        vector<int> cycle;
+
+        int current = i;
+
+        while (!visited[current]) {
+
+            visited[current] = true;
+            cycle.push_back(current);
+
+            current = changes[current] - 1;
+        }
+
+        int cycleLength = cycle.size();
+
+        for (int node : cycle) {
+            ans[node] = cycleLength;
+        }
     }
-    for (int day: totalDays){
-        cout << day << " ";
+
+    for (int x : ans) {
+        cout << x << " ";
     }
+
     cout << '\n';
 }
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
